c5dc5976823ba003e6f37edccfea59f8ef5045a3
mmuzahid <muzahid.engr@gmail.com>
2017-12-28 16:36:34 +0600

    Added method to query a document using SolrJ client

--- src/org/my/prac/PracticeSolr.java
+++ src/org/my/prac/PracticeSolr.java
@@ -16,6 +16,17 @@ public class PracticeSolr {
 		String urlString = "http://localhost:8983/solr/my-practice-core";
 		SolrClient solr = new HttpSolrClient.Builder(urlString).build();
 		System.out.println("Update Document response: " + updateSampleDocument(solr));
+		System.out.println("Query Document response:" + querySampleDocument(solr));
+	}
+
+	private static QueryResponse querySampleDocument(SolrClient solr)
+			throws SolrServerException, IOException {
+		SolrQuery query = new SolrQuery();
+		String searchKey = "id";
+		String searchValue = "55555";
+		query.set("q", searchKey + ":" + searchValue);
+		QueryResponse response = solr.query(query);
+		return response;
 	}
 
 	private static UpdateResponse updateSampleDocument(SolrClient solr)
